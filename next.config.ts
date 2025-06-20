ada6337438a0a0ca6828d67c632fcde71589b8c9
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-06-20 14:14:55 +0100

    Set basepath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/training-log-explorer',
+  assetPrefix: '/training-log-explorer',
   images: {
     unoptimized: true
   }
